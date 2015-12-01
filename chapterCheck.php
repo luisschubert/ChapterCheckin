189cb328b31055e640226ea7111d49abbcdaf01d
Luis Schubert <schubert.luis@me.com>
2015-11-30 22:31:56 -0800

    trying submission

--- chapterCheck.php
+++ chapterCheck.php
@@ -6,14 +6,16 @@
  * Time: 8:26 PM
  */
 $name = filter_input(INPUT_POST, "userid");
-$password = filter_input(INPUT_POST, "pwd");
-//print 'hello';
-//print $name;
-//print $password;
-//$currentTime =  date("Y-m-d@H:i:s");
 $myFile = "UserData.txt";
 $fh = fopen($myFile, 'a') or die("can't open file");
-$stringData = "Username: $name\n";
+$stringData = "Name: $name\n";
 fwrite($fh, $stringData);
 fclose($fh);
 ?>
+<html>
+<body>
+<p>
+    submitted
+</p>
+</body>
+</html>
